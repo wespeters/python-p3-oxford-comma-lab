81c3f71e044326af4fd47325b174741edcb40bf9
Wesley Peters <wesleypeters@gmail.com>
2023-07-04 14:45:23 -0600

    write function oxford_comma()

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,7 @@
 def oxford_comma(items):
-    return None
+    if len(items) < 2:
+        return ''.join(items)
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
